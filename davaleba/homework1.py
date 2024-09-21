40c0417d3298b4f6a6fe928610271eb19b2db347
Baramidze-luka <baramidzeluka50@gmail.com>
2024-09-21 19:47:44 +0400

    first commit

---
+++ davaleba/homework1.py
@@ -0,0 +1,12 @@
+
+
+name = "luka"
+surname = "baramidze"
+age = 15
+height = 167.5
+Know_Math = True
+chemi = "chemi simaglea"
+fullname = "me var luka baramidze"
+AGE = "wlis"
+
+print(fullname + " " + str(age)+ " "  + AGE + " " + chemi + " "  + str(height))
