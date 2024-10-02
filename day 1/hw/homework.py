567f0cc20eb919516ccd5d7366a4fd1b46ea7827
Baramidze-luka <baramidzeluka50@gmail.com>
2024-10-02 19:34:37 +0400

    first commit

---
+++ day 1/hw/homework.py
@@ -0,0 +1,31 @@
+name ="luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"luka" არის ცვლადის მნიშვნელობა
+surname="baramidze"
+
+print(name)
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "luka" #ეს არის str(string) tipis ცვლადი
+age = 22 # es aris int(integer) tipis cvladi
+height = 185.6 #es aris float tipis cvladi  (ათწილადი)  
+#boolean (bool) tipis cvladi
+Knows_programing = True
+is_ugly = False
+
+print(name + " " + surname)
+
+#სტრინგი არის ფრჩხილებში მოქცეული სიმბოლო
+
+
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(Knows_programing))
+
+print(name + " " + str(age))             
