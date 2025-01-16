cf69121942486fcdd729b68ca7224d2c4db33848
Baramidze-luka <baramidzeluka50@gmail.com>
2025-01-16 21:09:46 +0400

    first commit

--- day 30/hw/homework.py
+++ day 30/hw/homework.py
@@ -4,5 +4,5 @@
 my_list = [1,2,3,4,5,6,7,8,9,10]
 
 for i in range(len(my_list)):
-    if i % 2 == 1:
+    if my_list[i] % 2 == 0:
         print(my_list[i]) 
--- day 30/hw/homework2.py
+++ day 30/hw/homework2.py
@@ -4,7 +4,7 @@
 my_list = [1,2,3,4,5,6,7,8,9,10]
 
 for i in range(len(my_list)):
-    if i % 2 == 1:
+    if my_list[i] % 2 == 1:
         print(str(my_list[i]) + "ლუწია")
-    elif i % 2 == 0:
+    elif my_list[i] % 2 == 0:
         print(str(my_list[i]) + "კენტია")
